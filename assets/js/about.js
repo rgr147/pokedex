c1deaf3bc55b48f0ce140bf6291255e9a4e4e020
rgr147 <139695514+rgr147@users.noreply.github.com>
2025-02-25 23:53:30 -0300

    a funcionalidade de puxar as iformações do pokemon na página about.html, e ba aba de menu 'About'está funcionando. Falta configurar as abas 'base stats' 'evolution' e'moves'. Além de estilizar o about.css para puxar a cor de backgroud da section de acordo com o tipo de pokemon

--- assets/js/about.js
+++ assets/js/about.js
@@ -15,16 +15,40 @@ const genderHtml = document.getElementById("gender");
 const eggGroupsHtml = document.getElementById("eggGroups");
 const eggCicleHtml = document.getElementById("eggCicle");
 
+const backgroundPokemonHtml = document.getElementById("pokemon")
 
-pokeApiDetails.getNameParam(nameParam);
 
+pokeApiDetails.getDataPokeApi(nameParam);
+
+
+function createLiTypesHtml(types) {
+    //Adicionando os elementos de acordo com a quantidade de tipos que o pokemon possui
+    types.map((type) => {
+        const liTypePokemon = document.createElement("li");
+        liTypePokemon.className = "content__pokemon__details__type";
+        liTypePokemon.innerText = type;
+        typesHtml.appendChild(liTypePokemon);
+    });
+}
 
 function inputDetailsInHtml(poke) {
     nameHtml.innerText = poke.name;
-    numberHtml.innerText = poke.id;
-    // typesHtml.innerText = poke.types;
+    numberHtml.innerText = `#${poke.id}`;
+   
+    createLiTypesHtml(poke.types);
+   
+    backgroundPokemonHtml.id = `pokemon ${poke.type}`;
     spriteHtml.src = poke.sprite;
 
+    //preenchendo informações adicionais do pokemon
+    //About
+    heightHtml.innerText = `${poke.height} (${(poke.height*0.3048).toFixed(2)} metros)`;
+    weightHtml.innerText = `${poke.weight}lbs (${(poke.weight*0.453592).toFixed(2)} (kg)`;
+    abilitiesHtml.innerText = poke.abilities.join(", ");
+    genderHtml.innerText = poke.gender;
+    eggGroupsHtml.innerText = poke.eggGroups;
+    eggCicleHtml.innerText = poke.eggCicle;
+    specieHtml.innerText = poke.specie;
 }
 
 
--- assets/js/poke-api-about.js
+++ assets/js/poke-api-about.js
@@ -1,7 +1,7 @@
 const pokeApiDetails = {};
 
 //Função responsável por criar o o objeto pokemon com todas as informações necessárias, usando como base o objeto json recebido da função que a chamou.
-function convertPokeApiDetailToPokemonDetailed(jsonBody /**name,id,types,type,sprite,height,weight,abilities*/) {
+async function convertPokeApiDetailToPokemonDetailed(jsonBody /**name,id,types,type,sprite,height,weight,abilities*/) {
     const poke = new PokemonDetailed()
     
     poke.name = jsonBody.name;//buscando o nome do pokemon no PokeApi; 
@@ -13,74 +13,69 @@ function convertPokeApiDetailToPokemonDetailed(jsonBody /**name,id,types,type,sp
     poke.sprite = jsonBody.sprites.other.dream_world.front_default; //busanco a imagem do pokemon no PokeApi;
     poke.height = jsonBody.height; //buscando a altura do pokemon no PokeAPI;
     poke.weight = jsonBody.weight;//buscando o peso do pokemon no PokeAPI;
-    poke.abilities = jsonBody.abilities.map((abilitiesSlot) => abilitiesSlot.ability.name);//buscando as habilidades do pokemon no pokeApi
-    
-    // poke.gender = gender;
-    // poke.eggGroups = eggGroups;
-    // poke.eggCicle = eggCicle;
+    poke.abilities = jsonBody.abilities.map((abilitiesSlot) => abilitiesSlot.ability.name);//
+
+    const breeding = await pokeApiDetails.breedingInfo(poke.name);
+    const specie = await pokeApiDetails.getSpecie(poke.id);
+
+    poke.gender = breeding.gender;
+    poke.eggGroups = breeding.eggGroup;
+    poke.eggCicle = breeding.eggCicle;
+
+    poke.specie = specie;
 
     return poke;
 }
 
-// /*função responsável por puxar as informações de Breeding da API  PokeAPI */
-// pokeApiDetails.breedingInfo = (pokemonName) => {
-//     const url = `https://pokeapi.co/api/v2/pokemon-species/${pokemonName}`; 
+/*função responsável por puxar as informações de Breeding da API  PokeAPI */
+pokeApiDetails.breedingInfo = (pokemonName) => {
+    const url = `https://pokeapi.co/api/v2/pokemon-species/${pokemonName}`; 
 
-//     return fetch(url)    
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (responseJson) { 
-//         const genderRate = responseJson.gender_rate;
-//         const groups = responseJson.egg_groups.map(groupSlot => groupSlot.name);
+    return fetch(url)    
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (responseJson) { 
+            
+            const genderRate = responseJson.gender_rate;
+            const groups = responseJson.egg_groups.map(groupSlot => groupSlot.name);
+            
+            const eggGroup = groups[0];
+            const eggCicle = groups[1];
+
+            const femaleRate = genderRate === -1 ? 0 : (genderRate / 8) * 100;
+            const maleRate = genderRate === -1 ? 0 : 100 - femaleRate;
         
-//         const eggGroup = groups[0];
-//         const eggCicle = groups[1];
+            const gender = `♀️ ${femaleRate.toFixed(1)}% / ♂️ ${maleRate.toFixed(1)}%`;
+            // const eggCicle = eggGroups.map(group => group.name).join(", ");
 
-//         const femaleRate = genderRate === -1 ? 0 : (genderRate / 8) * 100;
-//         const maleRate = genderRate === -1 ? 0 : 100 - femaleRate;
+            return {
+                gender,
+                eggGroup,
+                eggCicle
+            }
+        })
+}
+
+pokeApiDetails.getSpecie = (idPokemon) => {
+    const url = `https://pokeapi.co/api/v2/pokemon-species/${idPokemon}/`;
     
-//         const gender = `♀️ ${femaleRate.toFixed(1)}% / ♂️ ${maleRate.toFixed(1)}%`;
-//         // const eggCicle = eggGroups.map(group => group.name).join(", ");
-
-//         return {
-//             gender,
-//             eggGroup,
-//             eggCicle
-//         }
-//     })
-// }
-
-// pokeApiDetails.getSpecie = (urlSpecies) => {
-//     const url = urlSpecies;
-
-//     /**requisição HTTP para Poke APi retornar uma Promise com o corpo da requiseção(em JSON) */
-//     return fetch(url)
-//         /**tratamento da Promise para aconverte-la em um objeto javascript */
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (responseJson) {
-//             return responseJson.genera[7].genus;
-//         })
-//         .catch(function (error) {
-//             console.error(`Erro ao solicitar a specie de pokemon: ${error}`);
-//         })
-// }
-
-// //função reponsável por realizar o get via http com a API PokeAPI e tratar a promise recebida para json
-// function getApiDetailsPokemon(namePokemonParam) {
-//     const url = `https://pokeapi.co/api/v2/pokemon/${namePokemonParam}`;
-
-//     return fetch(url)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(convertPokeApiDetailToPokemonDetailed)
-// }
+    /**requisição HTTP para Poke APi retornar uma Promise com o corpo da requiseção(em JSON) */
+    return fetch(url)
+        /**tratamento da Promise para aconverte-la em um objeto javascript */
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (responseJson) {
+            return responseJson.genera[7].genus;
+        })
+        .catch(function (error) {
+            console.error(`Erro ao solicitar a specie de pokemon: ${error}`);
+        })
+}
 
 //função responsável por receber o parametro "name" com o nome do pokemon passado via url e realizar o get via http com a API PokeAPI
-pokeApiDetails.getNameParam = (nameParam) => {
+pokeApiDetails.getDataPokeApi = (nameParam) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${nameParam}`;
     
     return fetch(url)
